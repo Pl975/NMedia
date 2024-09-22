b5514e5cf18d10d0952c8b1006483e6e244c4ba9
pm-sh <pmshuv975@gmail.com>
2024-09-22 15:37:08 +0300

    HW 06_recyclerview

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,48 +3,38 @@ package ru.netology.nmedia.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
+import androidx.recyclerview.widget.DiffUtil
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.PostService
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                content.text = post.content
-                published.text = post.published
-                ivLikes.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_baseline_favorite_24
-                    else R.drawable.ic_baseline_favorite_border_24
-                )
-
-                likeCount.text = PostService.ConvertCountToShortString(post.likes)
-                share.text = PostService.ConvertCountToShortString(post.shareCount)
-                visibility.text = PostService.ConvertCountToShortString(post.visibilityCount)
-
-            }
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostAdapter(
+            { post ->
+            viewModel.likeById(post.id)
+        },
+        onShareListener = { post ->
+            viewModel.shareById(post.id)
         }
+        )
 
-        binding.ivLikes.setOnClickListener {
-            viewModel.like()
-        }
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
 
-        binding.ivShares.setOnClickListener {
-            viewModel.share()
-        }
-
-        binding.ivVisibility.setOnClickListener {
-            viewModel.visibility()
         }
     }
-
 }
+
+
+
+
+
+
