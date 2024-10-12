29db35a339e8889dbb8b4d9dd7042ccabfe6571d
pm-sh <pmshuv975@gmail.com>
2024-10-12 14:27:08 +0300

    HW 09_navigation_tasks 1-2

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,7 +1,6 @@
 package ru.netology.nmedia.dto
 
 
-
 data class Post(
     val id: Long,
     val author: String,
@@ -11,5 +10,13 @@ data class Post(
     val shareCount: Int = 700,
     val visibilityCount: Int = 550_000,
     val likedByMe: Boolean,
-    val sharedByMe: Boolean
+    val sharedByMe: Boolean,
+    val views: Int = 0,
+    val video: PostVideo? = null
+)
+
+data class PostVideo(
+    val name: String = "",
+    val url: String = "",
+    val views: Int = 2
 )
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -3,6 +3,7 @@ package ru.netology.nmedia.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.dto.PostVideo
 
 class PostRepositoryInMemoryImpl : PostRepository {
 
@@ -79,9 +80,10 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
             likedByMe = false,
-            sharedByMe = false
+            sharedByMe = false,
+            video = PostVideo("Весна - открытый фильм","https://www.youtube.com/watch?v=WhWc3b3KhnY")
         ),
-    )
+    ).reversed()
 
     private val data = MutableLiveData(posts)
     override fun getAll(): LiveData<List<Post>> = data
@@ -111,8 +113,8 @@ class PostRepositoryInMemoryImpl : PostRepository {
     override fun save(post: Post) {
         posts = if (post.id == 0L) {
             listOf(post.copy(id = nextId++, published = "Now", author = "Netology")) + posts
-            } else {
-                posts.map { if (it.id != post.id) it else it.copy(content = post.content) }
+        } else {
+            posts.map { if (it.id != post.id) it else it.copy(content = post.content) }
         }
         data.value = posts
     }
